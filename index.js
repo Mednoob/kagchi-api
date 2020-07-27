a0c6ae32ea35cc35c7a38836eba2b6beae3644d0
KagChi <59391215+KagChi@users.noreply.github.com>
2020-07-27 22:02:35 +0700

    Update index.js

--- index.js
+++ index.js
@@ -54,7 +54,8 @@ async subreddit(reddit) {
 if (!reddit) throw Error('Please enter subreddit');
    const { body: subreddits } = await agent.get(url + reddit + "/hot.json")
   if (typeof reddit !== 'string') throw Error('The value of async function find isn\'t a string!');
-    const subreddit = subreddits.data[Math.floor(Math.random() * 1000)];
+    const subredditz = subreddits.data[Math.floor(Math.random() * 1000)];
+const subreddit = subreddits.data[0]
 return subreddit;
 };
 };
