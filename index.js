37514394bd81ef451358f50fe8963e72b705b0fb
カグチ <59391215+KagChi@users.noreply.github.com>
2020-09-20 13:02:05 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,50 +3,50 @@ const { author, version } = require("./package.json");
 let loli = require("./endpoints/loli.js");
 let anime = require("./endpoints/anime.js");
 let waifu = require("./endpoints/waifu.js");
-  async wikihow() {
+  async function wikihow() {
     const { body: wikihow } = await request.get(
       "https://kagchi-api.glitch.me/wikihow"
     );
     return wikihow;
   };
-  async coin() {
+  async function coin() {
     const { body: coin } = await request.get(
       "https://kagchi-api.glitch.me/coin"
     );
     return coin;
   };
-  async user() {
+  async function user() {
     const { body: user } = await request.get(
       "https://kagchi-api.glitch.me/users"
     );
     return user.users[0];
   };
-  async memeindo() {
+  async function memeindo() {
     const { body: memeindo } = await request.get(
       "https://kagchi-api.glitch.me/meme/indonesia"
     );
     return memeindo;
   };
-  async memes() {
+  async function memes() {
     const { body: memes } = await request.get(
       "https://kagchi-api.glitch.me/meme/memes"
     );
     return memes;
   };
-  async dankmemes() {
+  async function dankmemes() {
     const { body: dankmemes } = await request.get(
       "https://kagchi-api.glitch.me/meme/dankmemes"
     );
     return dankmemes;
   };
-  async coronamemes() {
+  async function coronamemes() {
     const { body: coronamemes } = await request.get(
       "https://kagchi-api.glitch.me/meme/coronamemes"
     );
     return coronamemes;
   };
 
-   async subreddit(reddit) {
+   async function subreddit(reddit) {
      if (!reddit) throw Error('Please enter subreddit');
       const { body: subreddit } = await request.get("https://kagchi-api.glitch.me/subreddit/" + reddit)
     return subreddit;
