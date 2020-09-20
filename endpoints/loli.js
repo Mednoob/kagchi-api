00a2d89efe18d8e2a01d913204a35bce992932fe
カグチ <59391215+KagChi@users.noreply.github.com>
2020-09-20 10:54:42 +0700

    Update loli.js

--- endpoints/loli.js
+++ endpoints/loli.js
@@ -1,22 +1,22 @@
 const axios = require("axios");
 
 async kawaii() {
-  const data = await axios.get("https://api.lolis.life/kawaii").then(res => res.data)
+  const data = await axios.get("https://api.lolis.life/kawaii").then(res => res.data.url)
  return data;
 }
 
 async random() {
-  const data = await axios.get("https://api.lolis.life/random?nsfw=false").then(res => res.data)
+  const data = await axios.get("https://api.lolis.life/random?nsfw=false").then(res => res.data.url)
  return data;
 }
 
 async neko() {
-  const data = await axios.get("https://api.lolis.life/neko").then(res => res.data)
+  const data = await axios.get("https://api.lolis.life/neko").then(res => res.data.url)
  return data;
 }
 
 async pat() {
-   const data = await axios.get("https://api.lolis.life/pat").then(res => res.data)
+   const data = await axios.get("https://api.lolis.life/pat").then(res => res.data.url)
   return data;
 }
 
